26cb0d2b1081e1089253f7b736c735a9648b4651
Yongwoo <yongwoo@inha.ac.kr>
2024-04-30 20:51:55 +0900

    boot + LUT

--- src/pke/examples/simple-lookup-table.cpp
+++ src/pke/examples/simple-lookup-table.cpp
@@ -38,7 +38,26 @@
 #include <complex>
 #include <fftw3.h>
 #include <chrono>
+#include <cassert>
 
+
+// Note: put any 2logDim-to-2logDim table you want to test here 
+std::vector<uint32_t> invsbox = {0x52,0x09,0x6a,0xd5,0x30,0x36,0xa5,0x38,0xbf,0x40,0xa3,0x9e,0x81,0xf3,0xd7,0xfb,
+                                0x7c,0xe3,0x39,0x82,0x9b,0x2f,0xff,0x87,0x34,0x8e,0x43,0x44,0xc4,0xde,0xe9,0xcb,
+                                0x54,0x7b,0x94,0x32,0xa6,0xc2,0x23,0x3d,0xee,0x4c,0x95,0x0b,0x42,0xfa,0xc3,0x4e,
+                                0x08,0x2e,0xa1,0x66,0x28,0xd9,0x24,0xb2,0x76,0x5b,0xa2,0x49,0x6d,0x8b,0xd1,0x25,
+                                0x72,0xf8,0xf6,0x64,0x86,0x68,0x98,0x16,0xd4,0xa4,0x5c,0xcc,0x5d,0x65,0xb6,0x92,
+                                0x6c,0x70,0x48,0x50,0xfd,0xed,0xb9,0xda,0x5e,0x15,0x46,0x57,0xa7,0x8d,0x9d,0x84,
+                                0x90,0xd8,0xab,0x00,0x8c,0xbc,0xd3,0x0a,0xf7,0xe4,0x58,0x05,0xb8,0xb3,0x45,0x06,
+                                0xd0,0x2c,0x1e,0x8f,0xca,0x3f,0x0f,0x02,0xc1,0xaf,0xbd,0x03,0x01,0x13,0x8a,0x6b,
+                                0x3a,0x91,0x11,0x41,0x4f,0x67,0xdc,0xea,0x97,0xf2,0xcf,0xce,0xf0,0xb4,0xe6,0x73,
+                                0x96,0xac,0x74,0x22,0xe7,0xad,0x35,0x85,0xe2,0xf9,0x37,0xe8,0x1c,0x75,0xdf,0x6e,
+                                0x47,0xf1,0x1a,0x71,0x1d,0x29,0xc5,0x89,0x6f,0xb7,0x62,0x0e,0xaa,0x18,0xbe,0x1b,
+                                0xfc,0x56,0x3e,0x4b,0xc6,0xd2,0x79,0x20,0x9a,0xdb,0xc0,0xfe,0x78,0xcd,0x5a,0xf4,
+                                0x1f,0xdd,0xa8,0x33,0x88,0x07,0xc7,0x31,0xb1,0x12,0x10,0x59,0x27,0x80,0xec,0x5f,
+                                0x60,0x51,0x7f,0xa9,0x19,0xb5,0x4a,0x0d,0x2d,0xe5,0x7a,0x9f,0x93,0xc9,0x9c,0xef,
+                                0xa0,0xe0,0x3b,0x4d,0xae,0x2a,0xf5,0xb0,0xc8,0xeb,0xbb,0x3c,0x83,0x53,0x99,0x61,
+                                0x17,0x2b,0x04,0x7e,0xba,0x77,0xd6,0x26,0xe1,0x69,0x14,0x63,0x55,0x21,0x0c,0x7d};
 
 using namespace lbcrypto;
 
@@ -280,24 +299,9 @@ void EvalLUTs2D(Ciphertext<DCRTPoly> &ct0, Ciphertext<DCRTPoly> &ct1,
     std::vector<Ciphertext<DCRTPoly>> b1(codedim - 1);
 
 
-    // find power basis
-    b0[0] = ct0, b1[0] = ct1;
-    for (size_t i = 2; i < codedim; i++){
-        auto i0 = i/2;
-        auto i1 = i - i0;
-        b0[i - 1] = cc->EvalMult(b0[i0 - 1], b0[i1 - 1]);
-        b1[i - 1] = cc->EvalMult(b1[i0 - 1], b1[i1 - 1]);
-    }
-
-    // match the level and depth
-    for (size_t i = 0; i < codedim/2; i++){ 
-        algo->AdjustLevelsAndDepthInPlace(b0[i], b0[codedim-2]);
-        algo->AdjustLevelsAndDepthInPlace(b1[i], b1[codedim-2]);
-    }
-
-    // find power basis
+    // find power basis (without using complex conjugate)
     // b0[0] = ct0, b1[0] = ct1;
-    // for (size_t i = 2; i <= codedim/2; i++){
+    // for (size_t i = 2; i < codedim; i++){
     //     auto i0 = i/2;
     //     auto i1 = i - i0;
     //     b0[i - 1] = cc->EvalMult(b0[i0 - 1], b0[i1 - 1]);
@@ -305,21 +309,32 @@ void EvalLUTs2D(Ciphertext<DCRTPoly> &ct0, Ciphertext<DCRTPoly> &ct1,
     // }
 
     // // match the level and depth
-    // for (size_t i = 0; i < codedim/2 - 1; i++){ 
-    //     algo->AdjustLevelsAndDepthInPlace(b0[i], b0[codedim/2 - 1]);
-    //     algo->AdjustLevelsAndDepthInPlace(b1[i], b1[codedim/2 - 1]);
+    // for (size_t i = 0; i < codedim/2; i++){ 
+    //     algo->AdjustLevelsAndDepthInPlace(b0[i], b0[codedim-2]);
+    //     algo->AdjustLevelsAndDepthInPlace(b1[i], b1[codedim-2]);
     // }
 
-    // for (size_t i = codedim/2 + 1; i < codedim; i++){
-    //     auto i0 = codedim - i;
+    // find power basis
+    b0[0] = ct0, b1[0] = ct1;
+    for (size_t i = 2; i <= codedim/2; i++){
+        auto i0 = i/2;
+        auto i1 = i - i0;
+        b0[i - 1] = cc->EvalMult(b0[i0 - 1], b0[i1 - 1]);
+        b1[i - 1] = cc->EvalMult(b1[i0 - 1], b1[i1 - 1]);
+    }
 
-    //     b0[i - 1] = cc->EvalConjugate(b0[i0 - 1]);
-    //     b1[i - 1] = cc->EvalConjugate(b1[i0 - 1]);
-    // }
+    // match the level and depth
+    for (size_t i = 0; i < codedim/2 - 1; i++){ 
+        algo->AdjustLevelsAndDepthInPlace(b0[i], b0[codedim/2 - 1]);
+        algo->AdjustLevelsAndDepthInPlace(b1[i], b1[codedim/2 - 1]);
+    }
 
-    // for (auto &ct : b0){
-    //     std::cout << "Level: " <<  ct->GetLevel() << std::endl;
-    // }
+    for (size_t i = codedim/2 + 1; i < codedim; i++){
+        auto i0 = codedim - i;
+
+        b0[i - 1] = cc->EvalConjugate(b0[i0 - 1]);
+        b1[i - 1] = cc->EvalConjugate(b1[i0 - 1]);
+    }
 
     // evaluate the polynomial
     for (auto &table : coeffs){
@@ -533,32 +548,79 @@ void testLUTNtoBN(){
     }
 }
 
+inline uint32_t presetCryptoContext(CryptoContext<DCRTPoly>& cc){
+    // Create the crypto context
+    uint32_t multDepth = 5;
+    uint32_t scaleModSize = 50;
+    CCParams<CryptoContextCKKSRNS> parameters;
+    parameters.SetMultiplicativeDepth(multDepth);
+    parameters.SetScalingModSize(scaleModSize);
+
+    cc = GenCryptoContext(parameters);
+
+    cc->Enable(PKE);
+    cc->Enable(KEYSWITCH);
+    cc->Enable(LEVELEDSHE);
+
+    return multDepth;
+}
+
+inline uint32_t presetCryptoContextBootstrap(CryptoContext<DCRTPoly>& cc){
+    CCParams<CryptoContextCKKSRNS> parameters;
+
+    // Create the crypto context
+    SecretKeyDist secretKeyDist = SPARSE_TERNARY;
+    parameters.SetSecretKeyDist(secretKeyDist);
+
+    parameters.SetSecurityLevel(HEStd_128_classic);
+    // parameters.SetRingDim(1 << 12);
+    parameters.SetNumLargeDigits(3);
+    parameters.SetKeySwitchTechnique(HYBRID);
+
+    ScalingTechnique rescaleTech = FLEXIBLEAUTO;
+    usint dcrtBits               = 50;
+    usint firstMod               = 55;
+
+    parameters.SetScalingModSize(dcrtBits);
+    parameters.SetScalingTechnique(rescaleTech);
+    parameters.SetFirstModSize(firstMod);
+
+    std::vector<uint32_t> levelBudget = {3, 3};
+    std::vector<uint32_t> bsgsDim = {0, 0};
+
+    uint32_t levelsAvailableAfterBootstrap = 5;
+    usint depth = levelsAvailableAfterBootstrap + FHECKKSRNS::GetBootstrapDepth(levelBudget, secretKeyDist);
+    parameters.SetMultiplicativeDepth(depth);
+
+    cc = GenCryptoContext(parameters);
+
+    if(cc->GetRingDimension() > (1<<16)){
+        std::cout << "Ring dimension is too large for this example: " << std::dec << cc->GetRingDimension() << std::endl;
+        exit(-1);
+    }
+
+    cc->Enable(PKE);
+    cc->Enable(KEYSWITCH);
+    cc->Enable(LEVELEDSHE);
+    cc->Enable(ADVANCEDSHE);
+    cc->Enable(FHE);
+    cc->EvalBootstrapSetup(levelBudget, bsgsDim, 1<<15);
+
+    return depth;
+}
+
 void testLUTANtoBN(){
     size_t logDim = 4;
     // size_t inA = 2, // inA > 2 is not implemented yet
     size_t outB = 2; 
     std::cout << "Test an-to-bn LUT" << std::endl;
-    // Note: put any 2logDim-to-2logDim table you want to test here 
-    std::vector<uint32_t> invsbox = {0x52,0x09,0x6a,0xd5,0x30,0x36,0xa5,0x38,0xbf,0x40,0xa3,0x9e,0x81,0xf3,0xd7,0xfb,
-                                    0x7c,0xe3,0x39,0x82,0x9b,0x2f,0xff,0x87,0x34,0x8e,0x43,0x44,0xc4,0xde,0xe9,0xcb,
-                                    0x54,0x7b,0x94,0x32,0xa6,0xc2,0x23,0x3d,0xee,0x4c,0x95,0x0b,0x42,0xfa,0xc3,0x4e,
-                                    0x08,0x2e,0xa1,0x66,0x28,0xd9,0x24,0xb2,0x76,0x5b,0xa2,0x49,0x6d,0x8b,0xd1,0x25,
-                                    0x72,0xf8,0xf6,0x64,0x86,0x68,0x98,0x16,0xd4,0xa4,0x5c,0xcc,0x5d,0x65,0xb6,0x92,
-                                    0x6c,0x70,0x48,0x50,0xfd,0xed,0xb9,0xda,0x5e,0x15,0x46,0x57,0xa7,0x8d,0x9d,0x84,
-                                    0x90,0xd8,0xab,0x00,0x8c,0xbc,0xd3,0x0a,0xf7,0xe4,0x58,0x05,0xb8,0xb3,0x45,0x06,
-                                    0xd0,0x2c,0x1e,0x8f,0xca,0x3f,0x0f,0x02,0xc1,0xaf,0xbd,0x03,0x01,0x13,0x8a,0x6b,
-                                    0x3a,0x91,0x11,0x41,0x4f,0x67,0xdc,0xea,0x97,0xf2,0xcf,0xce,0xf0,0xb4,0xe6,0x73,
-                                    0x96,0xac,0x74,0x22,0xe7,0xad,0x35,0x85,0xe2,0xf9,0x37,0xe8,0x1c,0x75,0xdf,0x6e,
-                                    0x47,0xf1,0x1a,0x71,0x1d,0x29,0xc5,0x89,0x6f,0xb7,0x62,0x0e,0xaa,0x18,0xbe,0x1b,
-                                    0xfc,0x56,0x3e,0x4b,0xc6,0xd2,0x79,0x20,0x9a,0xdb,0xc0,0xfe,0x78,0xcd,0x5a,0xf4,
-                                    0x1f,0xdd,0xa8,0x33,0x88,0x07,0xc7,0x31,0xb1,0x12,0x10,0x59,0x27,0x80,0xec,0x5f,
-                                    0x60,0x51,0x7f,0xa9,0x19,0xb5,0x4a,0x0d,0x2d,0xe5,0x7a,0x9f,0x93,0xc9,0x9c,0xef,
-                                    0xa0,0xe0,0x3b,0x4d,0xae,0x2a,0xf5,0xb0,0xc8,0xeb,0xbb,0x3c,0x83,0x53,0x99,0x61,
-                                    0x17,0x2b,0x04,0x7e,0xba,0x77,0xd6,0x26,0xe1,0x69,0x14,0x63,0x55,0x21,0x0c,0x7d};
+
     std::cout << "Input invsbox: \n====================\n";
-    for (size_t i = 0; i < invsbox.size(); i++){
+    std::cout << std::hex;
+    for (size_t i = 0; i < 16; i++){
         std::cout << i << "\t" << invsbox[i] << std::endl;
     }
+    std::cout << "..." << std::endl << std::endl;
 
     std::vector<std::vector<std::vector<std::complex<double>>>> coeffs2D(2);
     // divide tables by chunks of size logDim-bit
@@ -579,26 +641,28 @@ void testLUTANtoBN(){
     GetExponent(mInt1, logDim, mComplex1);
     
     // Create the crypto context
-    uint32_t multDepth = 6;
-    uint32_t scaleModSize = 50;
-    CCParams<CryptoContextCKKSRNS> parameters;
-    parameters.SetMultiplicativeDepth(multDepth);
-    parameters.SetScalingModSize(scaleModSize);
-
-    CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
-
-    cc->Enable(PKE);
-    cc->Enable(KEYSWITCH);
-    cc->Enable(LEVELEDSHE);
+    CryptoContext<DCRTPoly> cc;
+    uint32_t maxDepth = presetCryptoContextBootstrap(cc);
 
     auto keys = cc->KeyGen();
+    uint32_t numSlots = 1 << 15;
     cc->EvalMultKeyGen(keys.secretKey);
     cc->EvalConjugateKeyGen(keys.secretKey);
+    cc->EvalBootstrapKeyGen(keys.secretKey, numSlots);
 
-    Plaintext ptxt0 = cc->MakeCKKSPackedPlaintext(mComplex0);
-    Plaintext ptxt1 = cc->MakeCKKSPackedPlaintext(mComplex1);
+    Plaintext ptxt0 = cc->MakeCKKSPackedPlaintext(mComplex0, 1, maxDepth - 1);
+    Plaintext ptxt1 = cc->MakeCKKSPackedPlaintext(mComplex1, 1, maxDepth - 1);
     Ciphertext<DCRTPoly> ctxt0 = cc->Encrypt(keys.publicKey, ptxt0);
     Ciphertext<DCRTPoly> ctxt1 = cc->Encrypt(keys.publicKey, ptxt1);
+    auto b_start = std::chrono::high_resolution_clock::now();
+
+    ctxt0 = cc->EvalBootstrap(ctxt0);
+    ctxt1 = cc->EvalBootstrap(ctxt1);
+
+    auto b_end = std::chrono::high_resolution_clock::now();
+    auto b_duration = std::chrono::duration_cast<std::chrono::milliseconds>(b_end - b_start);
+    std::cout << "Time taken (boot 2 times): " << std::dec << b_duration.count() << " ms" << std::endl;
+    
     std::vector<Ciphertext<DCRTPoly>> ctxtout;
 
     // Evaluate the LUT and measure the time
@@ -608,10 +672,7 @@ void testLUTANtoBN(){
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Time taken: " << duration.count() << "ms" << std::endl;
-
-    std::cout << "Ct level after: " << ctxtout[0]->GetLevel() << "/" << parameters.GetMultiplicativeDepth() << std::endl;
-
+    std::cout << "Time taken (4x2-to-4x2 LUT): " << std::dec << duration.count() << " ms" << std::endl;
 
     std::vector<Plaintext> result(outB);
     for (size_t i = 0; i < outB; i++){
@@ -626,12 +687,13 @@ void testLUTANtoBN(){
 
     std::cout << "Output table: \n====================\n";
     std::cout << "In(L)\tIn(R)\tMerged\t|Out(R)\tOut(L)\tMerged\tExpected\n";
+    std::cout << std::hex;
     for (size_t i = 0; i < mInt0.size(); i++){
         std::cout << mInt0[i] << "\t" << mInt1[i] << "\t" << mInt0[i]*(1<<logDim) + mInt1[i] << 
                 "\t|" << intResult[0][i] << "\t" << intResult[1][i] << "\t" << intResult[0][i]+ intResult[1][i]*(1<<logDim)  <<
                 "\t" << invsbox[mInt0[i]*(1<<logDim) + mInt1[i]]<< std::endl;
     }
-    std::cout << "noise" << std::endl;
+    std::cout << "Outupt(int)\tEncrypted\tExpected" << std::endl;
     for (size_t i = 0; i < mInt0.size(); i++){
         std::cout << intResult[0][i] << "\t" << result[0]->GetCKKSPackedValue()[i] << "\t";
         // calculate and print exp(-2*pi* i / 16 *intResult[0][i])
