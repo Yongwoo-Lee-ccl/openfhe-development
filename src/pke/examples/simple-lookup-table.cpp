a3395ce82d9dbcdf8d7986de163270b6879bb147
yongwoo <yongwool@snu.ac.kr>
2024-05-15 20:14:18 +0900

    asymmetric LUT done

--- src/pke/examples/simple-lookup-table.cpp
+++ src/pke/examples/simple-lookup-table.cpp
@@ -310,7 +310,7 @@ void EvalLUTs2D(Ciphertext<DCRTPoly> &ct0, Ciphertext<DCRTPoly> &ct1,
 
     // evaluate the polynomial
     for (auto &table : coeffs){
-        // innerproduct with coefficients
+        // inner product with coefficients
         Ciphertext<DCRTPoly> tableEval;
         for (size_t i = 0; i < codedim; i++){
             // Evaluate i-th row
@@ -834,31 +834,28 @@ void testLUTANtoBN(){
 void testLUTANtoBNAsymmetric(){
     size_t outB = 2; 
 
-    size_t logDim[] = {4, 4}; // size of outB, input and output has same size.
-    
-    std::cout << "Test an-to-bn LUT" << std::endl;
+    size_t logDim[] = {4, 5}; // size of outB, input and output has same size.
 
-    std::cout << "Input invsbox: \n====================\n";
+    size_t inputLen = 0;
+    for (auto &n : logDim){
+       inputLen += n; 
+    }
+    std::cout << "inputLen: " << inputLen << "\n";
+    
+    std::cout << "Test 2n-to-bn LUT, Asymmetric with " << logDim[0] << " and " << logDim[1] << " bits inputs" << std::endl;
+
+    std::cout << "Input table: \n====================\n";
     // Note: put any 2logDim-to-2logDim table you want to test here 
-    std::vector<uint32_t> invsbox = {0x52,0x09,0x6a,0xd5,0x30,0x36,0xa5,0x38,0xbf,0x40,0xa3,0x9e,0x81,0xf3,0xd7,0xfb,
-                                    0x7c,0xe3,0x39,0x82,0x9b,0x2f,0xff,0x87,0x34,0x8e,0x43,0x44,0xc4,0xde,0xe9,0xcb,
-                                    0x54,0x7b,0x94,0x32,0xa6,0xc2,0x23,0x3d,0xee,0x4c,0x95,0x0b,0x42,0xfa,0xc3,0x4e,
-                                    0x08,0x2e,0xa1,0x66,0x28,0xd9,0x24,0xb2,0x76,0x5b,0xa2,0x49,0x6d,0x8b,0xd1,0x25,
-                                    0x72,0xf8,0xf6,0x64,0x86,0x68,0x98,0x16,0xd4,0xa4,0x5c,0xcc,0x5d,0x65,0xb6,0x92,
-                                    0x6c,0x70,0x48,0x50,0xfd,0xed,0xb9,0xda,0x5e,0x15,0x46,0x57,0xa7,0x8d,0x9d,0x84,
-                                    0x90,0xd8,0xab,0x00,0x8c,0xbc,0xd3,0x0a,0xf7,0xe4,0x58,0x05,0xb8,0xb3,0x45,0x06,
-                                    0xd0,0x2c,0x1e,0x8f,0xca,0x3f,0x0f,0x02,0xc1,0xaf,0xbd,0x03,0x01,0x13,0x8a,0x6b,
-                                    0x3a,0x91,0x11,0x41,0x4f,0x67,0xdc,0xea,0x97,0xf2,0xcf,0xce,0xf0,0xb4,0xe6,0x73,
-                                    0x96,0xac,0x74,0x22,0xe7,0xad,0x35,0x85,0xe2,0xf9,0x37,0xe8,0x1c,0x75,0xdf,0x6e,
-                                    0x47,0xf1,0x1a,0x71,0x1d,0x29,0xc5,0x89,0x6f,0xb7,0x62,0x0e,0xaa,0x18,0xbe,0x1b,
-                                    0xfc,0x56,0x3e,0x4b,0xc6,0xd2,0x79,0x20,0x9a,0xdb,0xc0,0xfe,0x78,0xcd,0x5a,0xf4,
-                                    0x1f,0xdd,0xa8,0x33,0x88,0x07,0xc7,0x31,0xb1,0x12,0x10,0x59,0x27,0x80,0xec,0x5f,
-                                    0x60,0x51,0x7f,0xa9,0x19,0xb5,0x4a,0x0d,0x2d,0xe5,0x7a,0x9f,0x93,0xc9,0x9c,0xef,
-                                    0xa0,0xe0,0x3b,0x4d,0xae,0x2a,0xf5,0xb0,0xc8,0xeb,0xbb,0x3c,0x83,0x53,0x99,0x61,
-                                    0x17,0x2b,0x04,0x7e,0xba,0x77,0xd6,0x26,0xe1,0x69,0x14,0x63,0x55,0x21,0x0c,0x7d};
+    std::vector<uint32_t> input_table(1<<inputLen);
+
+    for (size_t i = 0; i < 1<<inputLen; i++){ // put any cool table you want
+        // mod 5
+        input_table[i] = i % 5;
+    }
+
     std::cout << std::hex;
     for (size_t i = 0; i < 16; i++){
-        std::cout << i << "\t" << invsbox[i] << std::endl;
+        std::cout << i << "\t" << input_table[i] << std::endl;
     }
     std::cout << "..." << std::endl << std::endl;
 
@@ -867,11 +864,11 @@ void testLUTANtoBNAsymmetric(){
     size_t nShift = 0; 
     for (size_t i = 0; i < outB; i++){
         // std::cout << "Coeffs(int) for output " << i << std::endl;
-        std::vector<uint32_t> partialTable(invsbox.size());
+        std::vector<uint32_t> partialTable(input_table.size());
         uint32_t mask = (1<<logDim[i]) - 1;
         mask <<= nShift;
-        for (size_t j = 0; j < invsbox.size(); j++){
-            partialTable[j] = (invsbox[j] & mask) >> (nShift);
+        for (size_t j = 0; j < input_table.size(); j++){
+            partialTable[j] = (input_table[j] & mask) >> (nShift);
             // std::cout << partialTable[j] << "(" << invsbox[j] << ",  " << mask << ", " <<  (nShift)<< ") ";
         }
         // std::cout << std::endl;
@@ -900,7 +897,7 @@ void testLUTANtoBNAsymmetric(){
     std::cout << "Generating keys..." << std::endl;
     cc->EvalMultKeyGen(keys.secretKey);
     cc->EvalConjugateKeyGen(keys.secretKey);
-    cc->EvalBootstrapKeyGen(keys.secretKey, numSlots);
+    // cc->EvalBootstrapKeyGen(keys.secretKey, numSlots);
     std::cout << "Keys generated." << std::endl;
 
     // Encode coeffs
@@ -914,12 +911,6 @@ void testLUTANtoBNAsymmetric(){
             }
         }
     }
-
-    size_t inputLen = 0;
-    for (auto &n : logDim){
-       inputLen += n; 
-    }
-    std::cout << "inputLen: " << inputLen << "\n";
 
     std::vector<std::vector<uint32_t>> mInt(2);
     nShift = 0;
@@ -939,7 +930,7 @@ void testLUTANtoBNAsymmetric(){
     GetExponent(mInt[0], logDim[0], mComplex[0]);
     GetExponent(mInt[1], logDim[1], mComplex[1]);
 
-    uint32_t initLevel = 1;
+    uint32_t initLevel = 6;
 
     Plaintext ptxt0 = cc->MakeCKKSPackedPlaintext(mComplex[0], 1, maxDepth - initLevel);
     Plaintext ptxt1 = cc->MakeCKKSPackedPlaintext(mComplex[1], 1, maxDepth - initLevel);
@@ -947,8 +938,8 @@ void testLUTANtoBNAsymmetric(){
     Ciphertext<DCRTPoly> ctxt1 = cc->Encrypt(keys.publicKey, ptxt1);
     auto b_start = std::chrono::high_resolution_clock::now();
 
-    ctxt0 = cc->EvalBootstrap(ctxt0);
-    ctxt1 = cc->EvalBootstrap(ctxt1);
+    // ctxt0 = cc->EvalBootstrap(ctxt0);
+    // ctxt1 = cc->EvalBootstrap(ctxt1);
 
     auto b_end = std::chrono::high_resolution_clock::now();
     auto b_duration = std::chrono::duration_cast<std::chrono::milliseconds>(b_end - b_start);
@@ -963,7 +954,7 @@ void testLUTANtoBNAsymmetric(){
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Time taken (4x2-to-4x2 LUT): " << std::dec << duration.count() << " ms" << std::endl;
+    std::cout << "Time taken (" << logDim[0] << "+" << logDim[1] << ")-to-(" << logDim[0] << "+" << logDim[1] << ") LUT): " << std::dec << duration.count() << " ms" << std::endl;
 
     std::vector<Plaintext> result(outB);
     for (size_t i = 0; i < outB; i++){
@@ -982,7 +973,7 @@ void testLUTANtoBNAsymmetric(){
     for (size_t i = 0; i < 8; i++){
         std::cout << mInt[1][i] << "\t" << mInt[0][i] << "\t" << mInt[1][i]*(1<<logDim[0]) + mInt[0][i] << 
                 "\t|" << intResult[1][i] << "\t" << intResult[0][i] << "\t" << intResult[1][i]*(1<<logDim[0]) + intResult[0][i] <<
-                "\t" << invsbox[mInt[1][i]*(1<<logDim[0]) + mInt[0][i]]<< std::endl;
+                "\t" << input_table[mInt[1][i]*(1<<logDim[0]) + mInt[0][i]]<< std::endl;
     }
 
     std::vector<std::vector<uint32_t>> expected(outB);
@@ -992,7 +983,7 @@ void testLUTANtoBNAsymmetric(){
         uint32_t mask = (1<<logDim[i]) - 1;
         mask <<= nShift;
         for (size_t j = 0; j < numSlots; j++){
-            expected[i][j] = (invsbox[j % (invsbox.size())] & mask) >> (nShift);
+            expected[i][j] = (input_table[j % (input_table.size())] & mask) >> (nShift);
         }
         nShift += logDim[i];
     }
@@ -1002,7 +993,7 @@ void testLUTANtoBNAsymmetric(){
     for (size_t i = 0; i < 8; i++){
         std::cout << intResult[0][i] << "\t" << result[0]->GetCKKSPackedValue()[i] << "\t";
         // calculate and print exp(-2*pi* i / 16 *expected[0][i])
-        std::cout << expected[0][i] << "\t" << std::exp(std::complex<double>(0, -2 * M_PI * expected[0][i] / 16)) << std::endl;
+        std::cout << expected[0][i] << "\t" << std::exp(std::complex<double>(0, -2 * M_PI * expected[0][i] / (1 << logDim[0]))) << std::endl;
     }
 
     uint32_t count_failure = 0;
@@ -1022,8 +1013,8 @@ void testLUTANtoBNAsymmetric(){
     double variance = 0.;
     for (size_t i = 0; i < numSlots; i++){
         // variance of noise, noise: result[0]->GetCKKSPackedValue()[i] - std::exp(std::complex<double>(0, -2 * M_PI * expected[0][i] / 16))
-        variance += std::norm(result[0]->GetCKKSPackedValue()[i] - std::exp(std::complex<double>(0, -2 * M_PI * (int)expected[0][i] / 16)));
-        variance += std::norm(result[1]->GetCKKSPackedValue()[i] - std::exp(std::complex<double>(0, -2 * M_PI * (int)expected[1][i] / 16)));
+        variance += std::norm(result[0]->GetCKKSPackedValue()[i] - std::exp(std::complex<double>(0, -2 * M_PI * (int)expected[0][i] / (1 << logDim[0]))));
+        variance += std::norm(result[1]->GetCKKSPackedValue()[i] - std::exp(std::complex<double>(0, -2 * M_PI * (int)expected[1][i] / (1 << logDim[1]))));
     }
     std::cout << variance / (2*numSlots) << std::endl;
 }
